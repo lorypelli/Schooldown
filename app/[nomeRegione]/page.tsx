ad69ff42cd023b4440434b36386c0b7418f61b71
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-09-23 14:45:45 +0200

    Initial countdown process

--- app/[nomeRegione]/page.tsx
+++ app/[nomeRegione]/page.tsx
@@ -15,12 +15,49 @@ export default async function Home({ params }: { params: { nomeRegione: string }
     }
     const countdownInizio = Math.floor(res.inizioLezioni - Date.now() / 1000)
     const countdownFine = Math.floor(res.fineLezioni - Date.now() / 1000)
+    let mesi = 0
+    let settimane = 0
+    let giorni = 0
+    let ore = 0
+    let minuti = 0
+    let secondi = 0
+    let restoMesi = 0
+    let restoSettimane = 0
+    let restoGiorni = 0
+    let restoOre = 0
+    let restoMinuti = 0
+    if (countdownInizio < 0) {
+        mesi = Math.floor(countdownFine / (2.628 * 10**6))
+        restoMesi = Math.floor(countdownFine % (2.628 * 10**6))
+        settimane = Math.floor(restoMesi / (6.048 * 10**5))
+        restoSettimane = Math.floor(restoMesi % (6.048 * 10**5))
+        giorni = Math.floor(restoSettimane / (8.64 * 10**4))
+        restoGiorni = Math.floor(restoSettimane % (8.64 * 10**4))
+        ore = Math.floor(restoGiorni / (3.6 * 10**3))
+        restoOre = Math.floor(restoGiorni % (3.6 * 10**3))
+        minuti = Math.floor(restoOre / (6.0 * 10))
+        restoMinuti = Math.floor(restoOre % (6.0 * 10))
+        secondi = Math.floor(restoMinuti)
+    }
+    else {
+        mesi = Math.floor(countdownInizio / (2.628 * 10**6))
+        restoMesi = Math.floor(countdownInizio % (2.628 * 10**6))
+        settimane = Math.floor(restoMesi / (6.048 * 10**5))
+        restoSettimane = Math.floor(restoMesi % (6.048 * 10**5))
+        giorni = Math.floor(restoSettimane / (8.64 * 10**4))
+        restoGiorni = Math.floor(restoSettimane % (8.64 * 10**4))
+        ore = Math.floor(restoGiorni / (3.6 * 10**3))
+        restoOre = Math.floor(restoGiorni % (3.6 * 10**3))
+        minuti = Math.floor(restoOre / (6.0 * 10))
+        restoMinuti = Math.floor(restoOre % (6.0 * 10))
+        secondi = Math.floor(restoMinuti)
+    }
     return (
         <>
             <Navbar />
             <h1>{region.replaceAll("%20", " ")}</h1>
             <h1>{countdownInizio < 0 ? "La scuola finisce tra:" : "La scuola inizia tra:"}</h1>
-            <h1>{countdownInizio > 0 ? countdownInizio : countdownFine}</h1>
+            <h1>{`${mesi} mesi, ${settimane} settimane, ${giorni} giorni, ${ore} ore ${minuti} minuti ${secondi} secondi`}</h1>
         </>
     )
 }
